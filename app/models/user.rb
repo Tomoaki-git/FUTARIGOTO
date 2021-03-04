d637c624a78b2ab0f4fea4b1a94d2438642a82ea
Tomoaki-git <77450533+Tomoaki-git@users.noreply.github.com>
2021-03-04 16:19:13 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    with_options format:{ with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ } do
+      validates :password
+      validates :password_confirmation
+    end
+
+    validates :nickname
+  end
+
 end
