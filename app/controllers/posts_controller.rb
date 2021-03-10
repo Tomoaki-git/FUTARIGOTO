c089e3cedc7df54bb66af65a000281553a0497ab
Tomoaki-git <77450533+Tomoaki-git@users.noreply.github.com>
2021-03-10 12:06:52 +0900

    コントローラーの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,34 @@ class PostsController < ApplicationController
   end
 
   def index
-    
+    @post = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post.create
+    if @post.save
+      redirect_to :index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post)
+    .permit(
+      :title,
+      :content,
+      :first_choice,
+      :second_choice,
+      :third_choice,
+      :comment
+    )
+    .merge(user_id: current_user.id)
   end
 end
